653166f61b23ee67c3765e5b5e97200223841062
cgrad01 <cgrad01@gmail.com>
2017-07-21 15:46:45 -0400

    pass all factorial tests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,14 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute (int i) {
+        if(i < 0){
+            throw new IllegalArgumentException("Number must be positive");
+        }
+        if(i > 0){
+            return i * compute(i-1);
+        } else {
+            return 1;
+        }
     }
 }
